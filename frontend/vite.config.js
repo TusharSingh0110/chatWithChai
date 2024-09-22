9137676bfd42d27682e4bbc6e875420e10538b1c
Tushar-Singh1 <100280218+TusharSingh0110@users.noreply.github.com>
2024-09-22 12:39:20 +0530

    Deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000, 
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000",
+  server: {
+    port: 3000, // Frontend will run on port 3000
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // Proxy API requests to backend running on port 5000
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
